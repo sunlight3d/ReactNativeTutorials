e30206e0a2124ae6641c4e4db616debebceacf80
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-06-22 14:55:29 +0700

    update

--- React Native tutorial-11-FlatList and SectionList/code/TutorialProject/App.js
+++ React Native tutorial-11-FlatList and SectionList/code/TutorialProject/App.js
@@ -3,90 +3,65 @@ Nguyen Duc Hoang - sunlight4d@gmail.com
 React Native tutorial video
  */
 import React from 'react';
-import { AppRegistry, ScrollView, Image, Text } from 'react-native';
-
-export default class App extends React.Component {
+import { AppRegistry, View, Image, Text, FlatList, SectionList } from 'react-native';
+/*
+export default class FlatListComponent extends React.Component {
     constructor(props) {
         super(props);
     }
 
     render() {
+        var flowers = [
+            {key: "Lily"},
+            {key: "Holly"},
+            {key: "Jasmine"},
+            {key: "Daisy"},
+            {key: "Alyssum"},
+            {key: "Poppy"},
+            {key: "Violet"},
+            {key: "Ivy"}
+        ];
         return (
-            <ScrollView horizontal={true}>
-                <Text
-                    style={
-                        {
-                            fontSize: 30,
-                            padding: 20,
-                            backgroundColor: "red"
-                        }
-                    }>
-                    This is a text in a scrollView
-                </Text>
-                <Text
-                    style={
-                        {
-                            fontSize: 30,
-                            padding: 20,
-                            backgroundColor: "green"
-                        }
-                    }>
-                    This is second line
-                </Text>
-                <Image
-                    source={require('./images/my_display.jpg')}
-                    style={
-                        {
-                            width: 414,
-                            height: 414
-                        }
-                    }
-                    >
-                </Image>
-                <Text
-                    style={
-                        {
-                            fontSize: 30,
-                            padding: 20,
-                            backgroundColor: "yellow"
-                        }
-                    }>
-                    This is 3rd line
-                </Text>
-                <Image
-                    source={require('./images/my_display.jpg')}
-                    style={
-                        {
-                            width: 414,
-                            height: 414
-                        }
+            <View style={{flex:1, paddingTop: 30}}>
+                <FlatList
+                    data={flowers}
+                    renderItem={
+                        ({item}) => <Text style={{padding: 10, fontSize: 20, height: 44, color: "red"}}>Each item is: {item.key}</Text>
                     }
                 >
-                </Image>
-                <Text
-                    style={
-                        {
-                            fontSize: 30,
-                            padding: 50,
-                            backgroundColor: "blue"
-                        }
-                    }>
-                    This is line 4
-                </Text>
-                <Image
-                    source={require('./images/my_display.jpg')}
-                    style={
-                        {
-                            width: 414,
-                            height: 414
-                        }
-                    }
-                >
-                </Image>
-            </ScrollView>
+                </FlatList>
+            </View>
         );
     }
 }
+*/
+export default class SectionlistComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
+    render() {
+        var sectionData = [
+            {title: "A", data: [{key: "Alexander"}, {key: "Alan Turing"}], key: "A"},
+            {title: "B", data: [{key: "Brooke"}, {key: "Barbara"}, {key: "Brenda"}], key: "B"},
+            {title: "D", data: [{key: "David"}, {key: "Dylan"}, {key: "Diego"}, {key: "Dakota"}], key: "C"}
+        ];
+        return (
+            <View style={{flex:1, paddingTop: 30}}>
+                <SectionList
+                    sections={sectionData}
+                    renderItem={
+                        ({item}) => <Text style={{padding: 10, color:"green"}}>{item.key}</Text>
+                    }
+                    renderSectionHeader={
+                        ({section}) => <Text style={{padding: 10, color:"blue"}}>{section.title}</Text>
+                    }
+                >
+                </SectionList>
+            </View>
+        );
+    }
+}
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('Example of ScrollView', () => App);
+// AppRegistry.registerComponent('Example of FlatList', () => FlatListComponent);
+AppRegistry.registerComponent('Example of Sectionlist', () => SectionlistComponent);
